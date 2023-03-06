b6cfb8154dda9389839f6c8968a265c0efbf52f7
Justin Santiago <100609855+jtsanti@users.noreply.github.com>
2023-03-06 17:36:36 -0500

    add a rotating randsom color for the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  getRandomColor() {
+    return '#' + (Math.random() * 0xfffff * 1000000).toString(16).slice(0,6);
+  }
+
+  crazyColorChange(event: any) {
+    setInterval(() => {
+      event.target.style.color = this.getRandomColor();
+    }, 1000);
+  }
 }
