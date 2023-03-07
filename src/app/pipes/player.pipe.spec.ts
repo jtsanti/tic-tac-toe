9439117b620bcdde5a90003f9e62ce56331732e5
Justin Santiago <100609855+jtsanti@users.noreply.github.com>
2023-03-07 09:55:04 -0500

    Auto-generated commit [ci skip]

--- src/app/pipes/player.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlayerPipe } from './player.pipe';
-
-describe('PlayerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PlayerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
